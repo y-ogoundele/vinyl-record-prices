09d9af8d52b15967bba55b5ba627347bef6fcdf6
Yemi Ogoundele <yemi.ogoundele@hotmail.fr>
2022-01-07 21:57:54 +0100

    :lipstick: Linting

--- vinyl_record_scraper/discogs/api_fetcher.py
+++ vinyl_record_scraper/discogs/api_fetcher.py
@@ -1,9 +1,11 @@
-import yaml
-import requests
 import json
 import time
 
+import requests
+import yaml
+
 from discogs.api_config import base_url, discogs_endpoints
+
 
 def retrieve_discogs_credentials(path_to_credentials_file: str) -> dict:
     with open(path_to_credentials_file, "r") as creds:
@@ -11,13 +13,15 @@ def retrieve_discogs_credentials(path_to_credentials_file: str) -> dict:
             file_values = yaml.safe_load(creds)
             credentials = file_values['discogs-api']
         except yaml.YAMLError as exc:
-            raise(exc)
+            raise (exc)
         return credentials
+
 
 class DiscogsAPIFetcher:
     """
     Class to perform requests to the discogs HTTPS endpoint
     """
+
     def __init__(self, path_to_credentials_file):
         self.credentials = retrieve_discogs_credentials(path_to_credentials_file=path_to_credentials_file)
         self.base_url = base_url
@@ -53,7 +57,7 @@ class DiscogsAPIFetcher:
             if response.ok:
                 response_json = json.loads(response.text)
             else:
-                raise ValueError('API request failed with: %s'%response.text)
+                raise ValueError('API request failed with: %s' % response.text)
             accumulated_items.extend(response_json['releases'])
             if response_json['pagination']['pages'] == page_number:
                 all_items_pulled = True
